b9034a7d6b82be853bba165155e915bb946a248c
sunita <sunita@wtouch.in>
2015-08-10 14:11:18 +0530

    "index.html & app.js modified"

--- js/app.js
+++ js/app.js
@@ -6,39 +6,39 @@ var app = angular.module('hoc', [
 	'hoc.controllers'
 ]);
 app.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/', {templateUrl: 'template/login.html', controller: 'loginController'});
-	$routeProvider.when('/login', {templateUrl: 'template/login.html', controller: 'loginController'});
-	$routeProvider.when('/dashboard', {templateUrl: 'template/dashboard.html', controller: 'dashboardController'});
+	$routeProvider.when('/', {templateUrl: 'modules/login.html', controller: 'loginController'});
+	$routeProvider.when('/login', {templateUrl: 'modules/login.html', controller: 'loginController'});
+	$routeProvider.when('/dashboard', {templateUrl: 'modules/dashboard.html', controller: 'dashboardController'});
   //tax pages route
-	$routeProvider.when('/tax/:id?', {templateUrl: 'template/tax.html', controller: 'taxController'});
-	$routeProvider.when('/taxlist', {templateUrl: 'template/taxlist.html', controller: 'taxController'});
-	$routeProvider.when('/viewtax', {templateUrl: 'template/viewtax.html', controller: 'taxController'});
+	$routeProvider.when('/tax/:id?', {templateUrl: 'modules/tax.html', controller: 'taxController'});
+	$routeProvider.when('/taxlist', {templateUrl: 'modules/taxlist.html', controller: 'taxController'});
+	$routeProvider.when('/viewtax', {templateUrl: 'modules/viewtax.html', controller: 'taxController'});
   //summary route
-	$routeProvider.when('/summary', {templateUrl: 'template/summary.html', controller: 'summaryController'});
+	$routeProvider.when('/summary', {templateUrl: 'modules/summary.html', controller: 'summaryController'});
  //party pages route
-	$routeProvider.when('/party/:id?', {templateUrl: 'template/party.html', controller: 'partyController'});
-	$routeProvider.when('/partylist', {templateUrl: 'template/partylist.html', controller: 'partyController'});
-	$routeProvider.when('/viewparty', {templateUrl: 'template/viewparty.html', controller: 'partyController'});
+	$routeProvider.when('/party/:id?', {templateUrl: 'modules/party.html', controller: 'partyController'});
+	$routeProvider.when('/partylist', {templateUrl: 'modules/partylist.html', controller: 'partyController'});
+	$routeProvider.when('/viewparty', {templateUrl: 'modules/viewparty.html', controller: 'partyController'});
  //invoice pages route
-	$routeProvider.when('/invoice/:id?', {templateUrl: 'template/invoice.html', controller: 'invoiceController'});
-	$routeProvider.when('/invoicelist', {templateUrl: 'template/invoicelist.html', controller: 'invoiceController'});
-	$routeProvider.when('/viewinvoice', {templateUrl: 'template/viewinvoice.html', controller: 'invoiceController'});
+	$routeProvider.when('/invoice/:id?', {templateUrl: 'modules/invoice.html', controller: 'invoiceController'});
+	$routeProvider.when('/invoicelist', {templateUrl: 'modules/invoicelist.html', controller: 'invoiceController'});
+	$routeProvider.when('/viewinvoice', {templateUrl: 'modules/viewinvoice.html', controller: 'invoiceController'});
  //measurement pages route
-	$routeProvider.when('/measurement/:id?', {templateUrl: 'template/measurement.html', controller: 'measurementController'});
-	$routeProvider.when('/measurementlist', {templateUrl: 'template/measurementlist.html', controller: 'measurementController'});
-	$routeProvider.when('/viewmeasurement', {templateUrl: 'template/viewmeasurement.html', controller: 'measurementController'});
+	$routeProvider.when('/measurement/:id?', {templateUrl: 'modules/measurement.html', controller: 'measurementController'});
+	$routeProvider.when('/measurementlist', {templateUrl: 'modules/measurementlist.html', controller: 'measurementController'});
+	$routeProvider.when('/viewmeasurement', {templateUrl: 'modules/viewmeasurement.html', controller: 'measurementController'});
  //item pages route
-	$routeProvider.when('/item/:id?', {templateUrl: 'template/item.html', controller: 'itemController'});
-	$routeProvider.when('/itemlist', {templateUrl: 'template/itemlist.html', controller: 'itemController'});
-	$routeProvider.when('/viewitem', {templateUrl: 'template/viewitem.html', controller: 'itemController'});
+	$routeProvider.when('/item/:id?', {templateUrl: 'modules/item.html', controller: 'itemController'});
+	$routeProvider.when('/itemlist', {templateUrl: 'modules/itemlist.html', controller: 'itemController'});
+	$routeProvider.when('/viewitem', {templateUrl: 'modules/viewitem.html', controller: 'itemController'});
  //quotation pages route
-	$routeProvider.when('/quotation/:id?', {templateUrl: 'template/quotation.html', controller: 'quotationController'});
-	$routeProvider.when('/quotationlist', {templateUrl: 'template/quotationlist.html', controller: 'quotationController'});
-	$routeProvider.when('/viewquotation', {templateUrl: 'template/viewquotation.html', controller: 'quotationController'});
+	$routeProvider.when('/quotation/:id?', {templateUrl: 'modules/quotation.html', controller: 'quotationController'});
+	$routeProvider.when('/quotationlist', {templateUrl: 'modules/quotationlist.html', controller: 'quotationController'});
+	$routeProvider.when('/viewquotation', {templateUrl: 'modules/viewquotation.html', controller: 'quotationController'});
 //department pages route
-	$routeProvider.when('/department/:id?', {templateUrl: 'template/department.html', controller: 'departmentController'});
-	$routeProvider.when('/departmentlist', {templateUrl: 'template/departmentlist.html', controller: 'departmentController'});
-	$routeProvider.when('/viewdepartment', {templateUrl: 'template/viewdepartment.html', controller: 'departmentController'});
+	$routeProvider.when('/department/:id?', {templateUrl: 'modules/department.html', controller: 'departmentController'});
+	$routeProvider.when('/departmentlist', {templateUrl: 'modules/departmentlist.html', controller: 'departmentController'});
+	$routeProvider.when('/viewdepartment', {templateUrl: 'modules/viewdepartment.html', controller: 'departmentController'});
 	
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
