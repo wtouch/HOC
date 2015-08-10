d2932512328eb320f0b9190826d856dd72d271cb
sunita <sunita@wtouch.in>
2015-08-10 12:53:45 +0530

    "hoc app.js & controller.js created"

--- js/app.js
+++ js/app.js
@@ -1,21 +1,45 @@
 'use strict';
-// Declare app level module which depends on filters, and services
-var app = angular.module('patientApp', [
+
+var app = angular.module('hoc', [
 	'ngRoute',
 	'ui.bootstrap',
-	'patientApp.controllers'
+	'hoc.controllers'
 ]);
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/', {templateUrl: 'template/home.html'});
-  $routeProvider.when('/about', {templateUrl: 'template/about.html'});
-   $routeProvider.when('/contact', {templateUrl: 'template/contact.html'});
-  $routeProvider.when('/doctors', {templateUrl: 'template/doctors.html', controller: 'doctorController'});
-  $routeProvider.when('/viewdoctor', {templateUrl: 'template/viewdoctor.html', controller: 'appointmentController'});
-  $routeProvider.when('/appointment', {templateUrl: 'template/appointment.html', controller: 'appointmentController'});
-  $routeProvider.when('/viewappointment', {templateUrl: 'template/viewappointment.html', controller: 'viewController'});
-  $routeProvider.when('/news', {templateUrl: 'template/news.html', controller: 'appointmentController'});
-  $routeProvider.when('/services', {templateUrl: 'template/services.html', controller: 'appointmentController'});
-  
+	$routeProvider.when('/', {templateUrl: 'template/login.html', controller: 'loginController'});
+	$routeProvider.when('/login', {templateUrl: 'template/login.html', controller: 'loginController'});
+	$routeProvider.when('/dashboard', {templateUrl: 'template/dashboard.html', controller: 'dashboardController'});
+  //tax pages route
+	$routeProvider.when('/tax/:id?', {templateUrl: 'template/tax.html', controller: 'taxController'});
+	$routeProvider.when('/taxlist', {templateUrl: 'template/taxlist.html', controller: 'taxController'});
+	$routeProvider.when('/viewtax', {templateUrl: 'template/viewtax.html', controller: 'taxController'});
+  //summary route
+	$routeProvider.when('/summary', {templateUrl: 'template/summary.html', controller: 'summaryController'});
+ //party pages route
+	$routeProvider.when('/party/:id?', {templateUrl: 'template/party.html', controller: 'partyController'});
+	$routeProvider.when('/partylist', {templateUrl: 'template/partylist.html', controller: 'partyController'});
+	$routeProvider.when('/viewparty', {templateUrl: 'template/viewparty.html', controller: 'partyController'});
+ //invoice pages route
+	$routeProvider.when('/invoice/:id?', {templateUrl: 'template/invoice.html', controller: 'invoiceController'});
+	$routeProvider.when('/invoicelist', {templateUrl: 'template/invoicelist.html', controller: 'invoiceController'});
+	$routeProvider.when('/viewinvoice', {templateUrl: 'template/viewinvoice.html', controller: 'invoiceController'});
+ //measurement pages route
+	$routeProvider.when('/measurement/:id?', {templateUrl: 'template/measurement.html', controller: 'measurementController'});
+	$routeProvider.when('/measurementlist', {templateUrl: 'template/measurementlist.html', controller: 'measurementController'});
+	$routeProvider.when('/viewmeasurement', {templateUrl: 'template/viewmeasurement.html', controller: 'measurementController'});
+ //item pages route
+	$routeProvider.when('/item/:id?', {templateUrl: 'template/item.html', controller: 'itemController'});
+	$routeProvider.when('/itemlist', {templateUrl: 'template/itemlist.html', controller: 'itemController'});
+	$routeProvider.when('/viewitem', {templateUrl: 'template/viewitem.html', controller: 'itemController'});
+ //quotation pages route
+	$routeProvider.when('/quotation/:id?', {templateUrl: 'template/quotation.html', controller: 'quotationController'});
+	$routeProvider.when('/quotationlist', {templateUrl: 'template/quotationlist.html', controller: 'quotationController'});
+	$routeProvider.when('/viewquotation', {templateUrl: 'template/viewquotation.html', controller: 'quotationController'});
+//department pages route
+	$routeProvider.when('/department/:id?', {templateUrl: 'template/department.html', controller: 'departmentController'});
+	$routeProvider.when('/departmentlist', {templateUrl: 'template/departmentlist.html', controller: 'departmentController'});
+	$routeProvider.when('/viewdepartment', {templateUrl: 'template/viewdepartment.html', controller: 'departmentController'});
+	
   $routeProvider.otherwise({redirectTo: '/'});
 }]);
 
--- js/controller.js
+++ js/controller.js
@@ -3,111 +3,34 @@
 
 /* Controllers */
 
-angular.module('patientApp.controllers',[]);
-app.controller("appointmentController",function($scope,$http){
-	console.log("appointment controller");
-	$scope.oneAtATime = true;
-	$scope.status = {
-		isFirstOpen: true,
-		isFirstDisabled: false
-	};
-	$scope.isOpen =function(){
-		console.log("isopen");
-		$scope.status = {
-			isSecondOpen: true,
-			isFirstDisabled: false
-		}; 
-		
-	}
-	$scope.status.open = true;
-	$scope.today = function() {
-    $scope.dt = new Date();
-  };
-	
-	var cdate= new Date();
-	var dd = cdate.getDate();
-	var mm = cdate.getMonth()+1;
-	var yy = cdate.getFullYear();
-	$scope.myDate =dd + '/' +mm +'/'+yy;
-	console.log($scope.myDate);
-	$scope.clear = function () {
-		$scope.dt = null;
-	};
-/************************* code for date selection **********************************/
-	$scope.today();
-	  // Disable weekend selection
-	$scope.disabled = function(date, mode) {
-		return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-	};
-
-	$scope.toggleMin = function() {
-		$scope.minDate = $scope.minDate ? null : new Date();
-	};
-	$scope.toggleMin();
-
-	$scope.open = function($event) {
-		$event.preventDefault();
-		$event.stopPropagation();
-		$scope.opened = true;
-	};
-
-	$scope.dateOptions = {
-		formatYear: 'yy',
-		startingDay: 1
-	};
-	
-	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-	$scope.format = $scope.formats[0];
-	
-/********************************************************************************************/
-	$scope.insert = function(appointment){
-		console.log(appointment);
-		//$scope.appointment.cdate = $scope.myDate;
-		$http.post("http://localhost/sunita/patientapp/server.php/post",$scope.appointment).success(function(response){
-			console.log(response);
-		})
-		console.log(appointment);
-	}
-
+angular.module('hoc.controllers',[]);
+app.controller("loginController",function($scope,$http){
+	console.log("this is login controller");
 });
-app.controller("doctorController",function($scope,$http,$modal,$location){
-	
-/************************ code for open modal for doctor single view*************************/	
-	$scope.open = function () {
-		size = 'lg';
-		$modal.open({
-			templateUrl: 'template/viewdoctor.html',
-			size: size,
-			
-		});
-		
-	}
-	$scope.ok = function () {
-		$modal.close();
-	};
-	$scope.cancel = function () {
-		$modal.dismiss();
-	};
-	
-	$scope.login = function(logindata){
-		console.log(logindata);
-		var n = "sai";
-		var p = "ram";
-		var name = $scope.logindata.name;
-		var pass = $scope.logindata.password;
-		console.log(name);
-		console.log(pass);
-		
-		if((name == n) && (pass == p )){
-			alert("login successful");
-			$location.path("/viewappointment");
-		}else{
-			
-			alert("Invalid password");
-		}
-	}
+app.controller("dashboardController",function($scope,$http){
+	console.log("this is dashboard controller");
 });
-app.controller('ViewController', function($scope,$http) {
-	$http.get("http://localhost/sunita/patientapp/server.php/get").success(function(response) {	$scope.patient = response;
-	});
+app.controller("taxController",function($scope,$http){
+	console.log("this is tax controller");
+});
+app.controller("summaryController",function($scope,$http){
+	console.log("this is summary controller");
+});
+app.controller("partyController",function($scope,$http){
+	console.log("this is party controller");
+});
+app.controller("invoiceController",function($scope,$http){
+	console.log("this is invoice controller");
+});
+app.controller("measurementController",function($scope,$http){
+	console.log("this is measurement controller");
+});
+app.controller("itemController",function($scope,$http){
+	console.log("this is item controller");
+});
+app.controller("quotationController",function($scope,$http){
+	console.log("this is quotation controller");
+});
+app.controller("departmentController",function($scope,$http){
+	console.log("this is depertment controller");
 });
