92624d92aef820f76206d1fbe36ea2e3fc78ee38
sunita <sunita@wtouch.in>
2015-08-22 18:00:02 +0530

    "measurement.html ,
    measurementController.js,
    measurementlist.html modified"

--- app/modules/measurement/measurementController.js
+++ app/modules/measurement/measurementController.js
@@ -9,65 +9,130 @@ var injectParams = ['$scope', '$injector','$routeParams','$rootScope','dataServi
 		$scope.currentPage = 1;
 		$scope.pageItems = 10;
 		$scope.alerts = [];
+		$scope.currDate = dataService.currentDate;
 		
 		// function to close alert
 		$scope.closeAlert = function(index) {
 			$scope.alerts.splice(index, 1);
 		};
 		
-		$scope.openAddMeasurement = function (addmeasurement) {
-			console.log(addmeasurement);
-			var x = angular.copy(addmeasurement);
-			var modalDefaults = {
-				templateUrl: 'modules/measurement/measurement.html',	
-				size : 'lg'
-			};
-			var modalOptions = {
-				addmeasurement : (addmeasurement) ? x :{},
+		$scope.getMeasurement = function(page, params){
 				
-				postData : function(addmeasurement) {
-					dataService.post("measurement", addmeasurement).then(function(response) {
-						console.log(response);
-						if(response.status == "success"){
-							$scope.getMeasurement($scope.currentPage, $scope.params);
-						}
-						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
-						$notification[response.status](" Record Added", response.message);
-					});
-					
-				}, 
-				updateData : function(addmeasurement) {
-					var params = {where:{id:addmeasurement.id}};
-					console.log(params);
-					delete addmeasurement.id;
-					console.log(addmeasurement);
-					dataService.put("measurement",addmeasurement,params)
-					.then(function(response) {
-						console.log(response);
-						if(response.status == "success"){
-							$scope.getMeasurement($scope.currentPage, $scope.params);
-						}
-						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
-						$notification[response.status]("Update record", response.message);
-					});
+			$scope.params = (params) ? params : {
+			 where : {
+					status : 1
 				}
-			};
-			modalService.showModal(modalDefaults,modalOptions).then(function (result) {
-			});
-		};
-		$scope.viewMeasurement = function (measurementdata) {
-			var modalDefaults = {
-				templateUrl: 'modules/measurement/viewmeasurement.html',	
-				size : 'lg'
-			};
-			var modalOptions = {
-				measurement : measurementdata,
-			};
-			modalService.showModal(modalDefaults,modalOptions).then(function (result) {
+			}; 
+			angular.extend($scope.params, {limit : {
+					page : page,
+					records : $scope.pageItems
+				}
+			})
+			dataService.get(false,"measurement", $scope.params)
+			.then(function(response) {
 			
+				if(response.status == 'success'){
+					$scope.measurementlist = angular.copy(response.data);
+					console.log($scope.measurementlist);
+					$scope.totalRecords = response.totalRecords;
+				}else{
+					$scope.measurementlist = [];
+					$scope.totalRecords = 0;
+					if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+					$notification[response.status]("Get Transactions", response.message);
+				}
 			});
-		};
-		console.log("this is measurement controller");
+		}
+			$scope.openAddMeasurement = function (addmeasurement) {
+				console.log(addmeasurement);
+				var x = angular.copy(addmeasurement);
+				var modalDefaults = {
+					templateUrl: 'modules/measurement/measurement.html',	
+					size : 'lg'
+				};
+				var modalOptions = {
+					addmeasurement : (addmeasurement) ? x :{},
+					postData : function(addmeasurement) {
+						dataService.post("measurement", addmeasurement).then(function(response){
+							if(response.status == "success"){
+								$scope.getMeasurement($scope.currentPage, $scope.params);
+							}
+							if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+							$notification[response.status]("Add record", response.message);
+						});;
+						console.log(addmeasurement); 
+					},
+					updateData : function(addmeasurement) {
+						var params = {where:{id:addmeasurement.id}};
+						console.log(params);
+						delete addmeasurement.id;
+						console.log(addmeasurement);
+						dataService.put("measurement",addmeasurement,params)
+						.then(function(response) {
+							console.log(response);
+							if(response.status == "success"){
+								$scope.getMeasurement($scope.currentPage, $scope.params);
+							}
+							if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+							$notification[response.status]("Update record", response.message);
+						});
+					},
+					getData : function(table, modalOptions, subobj) {
+						console.log(modalOptions);
+						$scope.params = {
+							where : {
+								status : 1
+							}
+						};
+						
+						dataService.get(false,table,$scope.params)
+						.then(function(response) {
+							console.log(response);
+							if(response.status == "success"){
+								modalOptions[subobj] = response.data;
+							}
+						});
+					},
+				};
+				modalService.showModal(modalDefaults,modalOptions).then(function (result) {
+				});
+			};
+			$scope.viewMeasurement = function (measurementdata) {
+				var modalDefaults = {
+					templateUrl: 'modules/measurement/viewmeasurement.html',	
+					size : 'lg'
+				};
+				var modalOptions = {
+					measurement : measurementdata,
+				};
+				modalService.showModal(modalDefaults,modalOptions).then(function (result) {
+				
+				});
+			};
+			
+			$scope.getParty = function(){
+				$scope.params = {
+					where : {
+						status : 1
+					}
+					
+				};
+				
+				dataService.get(false,"party", $scope.params)
+				.then(function(response) {
+					
+					if(response.status == 'success'){
+						$scope.partylist = response.data;
+						console.log($scope.partylist);
+						$scope.totalRecords = response.totalRecords;
+					}else{
+						$scope.partylist = [];
+						$scope.totalRecords = 0;
+						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
+						$notification[response.status]("Get Transactions", response.message);
+					}
+				});
+			}
 			$scope.today = function() {
 				$scope.date = new Date();
 			};
@@ -81,31 +146,7 @@ var injectParams = ['$scope', '$injector','$routeParams','$rootScope','dataServi
 				$event.stopPropagation();
 				$scope.opened1 = ($scope.opened==true)?false:true;
 			};
-			$scope.getMeasurement = function(page, params){
-				$scope.params = (params) ? params : {};
-				/* where : {
-					status : 1
-				}
-			}; */
-				angular.extend($scope.params, {limit : {
-						page : page,
-						records : $scope.pageItems
-					}
-				})
-				dataService.get(false,"measurement", $scope.params)
-				.then(function(response) {
-					console.log(response);
-					if(response.status == 'success'){
-						$scope.measurementlist = angular.copy(response.data);
-						$scope.totalRecords = response.totalRecords;
-					}else{
-						$scope.measurementlist = [];
-						$scope.totalRecords = 0;
-						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
-						$notification[response.status]("Get Transactions", response.message);
-					}
-				});
-			}
+			
 			$scope.filter = function(col, value, search){
 				if(search == true){
 					if(!$scope.params.search) $scope.params.search = {};
@@ -139,32 +180,7 @@ var injectParams = ['$scope', '$injector','$routeParams','$rootScope','dataServi
 				return (status==1) ? active : notActive;
 			};
 			
-			//function to get party & department type
-			$scope.getParty = function(page, params){
-				$scope.params = (params) ? params : {};
-					/* where : {
-						status : 1
-					}
-				}; */
-				angular.extend($scope.params, {limit : {
-						page : page,
-						records : $scope.pageItems
-					}
-				})
-				dataService.get(false,"party", $scope.params)
-				.then(function(response) {
-					console.log(response)
-					if(response.status == 'success'){
-						$scope.partylist = angular.copy(response.data);
-						console.log($scope.partylist);
-					}else{
-						$scope.partylist = [];
-						
-						if(response.status == undefined) response = {status :"error", message:"Unknown Error"};
-						$notification[response.status]("Get Transactions", response.message);
-					}
-				});
-			}
+			
 	 };		 
 	// Inject controller's dependencies
 	measurementController.$inject = injectParams;
